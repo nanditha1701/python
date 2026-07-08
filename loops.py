c26043f66b4855899f89381a847384af18f8379c
nandithane <nenanditha@gmail.com>
2026-07-08 23:01:25 +0530

    first commit

---
+++ loops.py
@@ -0,0 +1,94 @@
+# num = int(input("Enter a number: "))
+# if num == 0:
+#     print("Your number is zero.")
+# elif num> 0:
+#     print("your number is greater than zero")
+# else :
+#     print("your number is lesser than zero")
+
+# num = int(input("enter a number: "))
+# if num % 2 == 0:
+#     print("your number is even")
+# else:
+#     print("your number is odd")
+
+# num1 = int(input("enter the number1"))
+# num2 = int(input("enter the number 2"))
+# if num1>num2:
+#     print(f"your number {num1} is greater then {num2}")
+# else:
+#     print(f"your number {num2} is greater then {num1}")
+
+
+
+# num1 = int(input("enter the number1"))
+# num2 = int(input("enter the number 2"))
+# num3 = int(input("enter the number 3"))
+# if num1 >num2 and num1>num3:
+#     print(num1,"is the greater num")
+# elif(num2>num1 and num2>num3):
+#     print(num2, "is the greater number")
+# else:
+#     print(num3, "is the greater number")
+
+# year = int(input("enter the year: "))
+# if year %4 ==0 and year %100 !=0 :
+#     print("year is leap year")
+# elif year %400 ==0:
+#     print("year is leap year")
+# else:
+#     print("year is not leap year")
+
+
+
+# age = int(input(" enter your age"))
+# if age >=18:
+#     print("you are eligible to vote")
+# else:
+#     print("you are a minor")
+
+# marks = int(input("enter your marks"))
+# if marks>=90:
+#     print("A")
+# elif marks >=80 and marks <90:
+#     print("B")
+# elif marks >=70 and marks <80:
+#     print("C")
+# elif marks >=60 and marks <70:
+#     print("D")
+# else:
+#     print("F")
+
+# NUM = int(input("enter a number: "))
+# if NUM % 5 ==0 and NUM %11 ==0:
+#     print("number is divisible by both 5 and 11")
+# else:
+#     print("the number is not divisible by 5 and 11")
+
+
+# ch = input("enter a character:")
+# if ch.isalpha():
+#     print("character is a alphabet")
+# elif ch.isdigit():
+#     print("character is a digit")
+# else:
+#     print("ch is a special character")
+
+x = int(input("enter a number: "))
+y = int(input("enter a number: "))
+op = input("enter an operator: ")
+if op == "+":
+    print(x+y)
+elif op =="-":
+    print(x-y)
+elif op =="*":
+    print(x*y)
+elif op =="/":
+    print(x/y)
+elif op == "%":
+    print(x%y)
+else:
+    print("invalid operator")
+
+
+
